209a97c7b524c70447a415cf24ef7bafc8eeb09b
ard-jan <ardjanaalberts@hotmail.com>
2016-09-26 15:22:40 +0200

    Added average cpu data grip, added fontawesome and changed menu

--- AuroraPlus/views.py
+++ AuroraPlus/views.py
@@ -1,36 +1,52 @@
 from django.shortcuts import render, render_to_response
 import json
-
-from django.contrib.auth.models import User
-import arrow
+import array
+from pprint import pprint
 
 from django.views.generic import TemplateView
 
 # Create your views here.
 from django.template import RequestContext
 
+with open('./testing/data.json', 'r') as f:
+    json_data = json.load(f)
 
-with open('./testing/names.json') as names_file:
-    names = json.load(names_file)
-    print names
+    for item in json_data['serverlist']['serverdata']:
 
-with open('./testing/data.json') as data_file:
-    data = json.load(data_file)
-    print data
+        servernames = item['servername']
+        print servernames
+
+        serverinfo = item['serverinfo']
+        print serverinfo
+
+        serverdata = item['serverdata']
+        print serverdata
+
+        servercpu = item['servercpu']
+        print servercpu
 
 
 def index(request):
-    decoded_data = data
-    decoded_names = names
-    return render_to_response('index.html', {'server_names': decoded_names, 'server_data': decoded_data})
+    print serverinfo
+    print serverdata
+    return render_to_response('index.html', {'names': servernames, 'info': serverinfo, 'data': serverdata,
+                                             'cpu': servercpu})
 
 
 def server_page(request, id):
-    decoded_data = data
-    decoded_names = names
     req = id
     print req
-    return render_to_response('server.html', {'server_names': decoded_names, 'server_data': decoded_data, 'id': req})
+    chart_data = "0,30;0,45;1,12"
+    escaped = chart_data.replace(",", ".")
+    splitted = escaped.replace(";", ",")
+    final = splitted.split(',')
+    item1 = float(final[0]) * 100
+    item2 = float(final[1]) * 100
+    item3 = float(final[2]) * 100
+    data_array = [item1, item2, item3]
+    print data_array
+    return render_to_response('server.html', {'id': req, 'names': servernames, 'info': serverinfo, 'data': serverdata,
+                                              'cpu': servercpu})
 
 
 def test(request):
