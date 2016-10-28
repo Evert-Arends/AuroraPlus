fb57a71bfc887c3cc81718bfa8e4d0f082f28f3b
ard-jan <ardjanaalberts@hotmail.com>
2016-10-28 11:44:20 +0200

    Started login functions. Not working yet.

--- Aurora/urls.py
+++ Aurora/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^server/(?P<server_id>\w{0,50})$', views.server_page, name='server'),
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^test/', views.test, name='test'),
 ]
