52226ba76ca34529a1ef27161c1809adae466537
ard-jan <ardjanaalberts@hotmail.com>
2016-11-21 09:57:45 +0100

    edited test templates and views.py

--- AuroraPlus/views.py
+++ AuroraPlus/views.py
@@ -1,4 +1,6 @@
 # imports
+from array import array
+
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from bin import cpu, jsondata, collector
@@ -38,33 +40,13 @@ def server_page(request, server_id):
 
 
 def test(request):
-    image = LandingPageImages(
-        PictureLinkName="image1",
-        PictureLink="http://imgur.com/Sdy6Vq0.png",
-        DescText="Clean overview of all your servers and all the necessary data you need. "
-                 "We designed it to be clean, understandable and modern looking."
-    )
+    images = LandingPageImages.objects.all().order_by('ID')
 
-    image.save()
+    for image in images:
+        images_array = image.ID, image.PictureLink, image.DescText
 
-    image2 = LandingPageImages(
-        PictureLinkName="image2",
-        PictureLink="http://imgur.com/VgE7yXo.png",
-        DescText="In the dashboard menu you can choose the server you want to see. "
-                 "In this case server 1, server 2 en server 3."
-    )
-
-    image2.save()
-
-    image3 = LandingPageImages(
-        PictureLinkName="image3",
-        PictureLink="http://imgur.com/qIxUbVi.png",
-        DescText="Once on the server page, the details are displayed in the graphics. "
-                 "Monitoring your servers was never this easy!"
-    )
-
-    image3.save()
-    return render_to_response('test.html')
+    print images_array
+    return render_to_response('test.html', {'images': images})
 
 
 def landing_page(request):
