dfe83a90ce02fab9cf1fb4ff8c37ae979c76a500
Evert-Arends <evert.arends98@gmail.com>
2016-11-25 13:27:12 +0100

    Progress in #19

--- bin/ServerManaging/manage.py
+++ bin/ServerManaging/manage.py
@@ -16,10 +16,24 @@ class ManageServer:
         if name is None or key is None or address is None or user_id is None:
             return 'Not all variables are fulfilled.'
         else:
+
             new_server_entry = Servers(User_ID=user_id, Server_Name=name, Server_key=key,
                                        Server_Description=description)
             new_server_entry.save()
             return True
+
+    @staticmethod
+    def check_if_name_is_unique(name, user_id):
+        if name:
+            check = Servers.objects.filter(
+                Server_Name=name,
+                User_ID=user_id
+            )
+            if check:
+                return
+            else:
+                return True
+
 
 if __name__ == '__main__':
     add_server = ManageServer()
